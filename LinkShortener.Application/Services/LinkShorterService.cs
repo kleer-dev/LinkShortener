044ac94e1d64be10aac5425f2809467cd5a881f1
KleerZ <ivan.kucherov.job@gmail.com>
2023-11-23 17:10:26 +0300

    add design time db context factory

--- LinkShortener.Application/Services/LinkShorterService.cs
+++ LinkShortener.Application/Services/LinkShorterService.cs
@@ -14,11 +14,7 @@ public class LinkShorterService : ILinkShorterService
 
         for (var i = 0; i < inputBytes.Length; i++)
             encryptedBytes[i] = (byte)(inputBytes[i] ^ keyBytes[i % keyBytes.Length]);
-        
-        var limitedBytes = new byte[length];
-        Array.Copy(encryptedBytes, encryptedBytes.Length - length, 
-            limitedBytes, 0, length);
 
-        return Convert.ToBase64String(limitedBytes);
+        return Convert.ToBase64String(encryptedBytes)[..length];
     }
 }
